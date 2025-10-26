febb2f93169341854a558747789e7b42e82963e5
RenanChacon <renan.victor.chacon@gmail.com>
2025-10-26 20:34:58 -0300

    Adicionado testes para o model Post

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+class TestPostModel(TestCase):
+
+    def setUp(self):
+      
+        self.post = Post.objects.create(
+            titulo="Post 1",
+            conteudo="Conteúdo do post 1."
+        )
+
+    def test_post_criacao(self):
+    
+        post_do_banco = Post.objects.get(id=self.post.id)
+        
+        self.assertEqual(post_do_banco.titulo, "Post 1")
+        self.assertEqual(post_do_banco.conteudo, "Conteúdo do post 1.")
+        
+        self.assertEqual(Post.objects.count(), 1)
+
+    def test_str_method(self):
+
+        post_do_banco = Post.objects.get(id=self.post.id)
+        
+        self.assertEqual(str(post_do_banco), "Post 1")
